9842b6f63e596b74091e6c20641ebc110f32ec0b
Daniil <danast_2@mail.ru>
2025-06-06 21:33:37 +0300

    first commit

---
+++ FinApp/FinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinAppApp.swift
+//  FinApp
+//
+//  Created by Даниил Дементьев on 06.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
